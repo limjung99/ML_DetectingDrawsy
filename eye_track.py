9c13461f6508ba8cdb1ab41b20e85f77ca9ecc15
westofsky <westofsky159@gmail.com>
2022-11-20 18:12:23 +0900

    Add eye_track & Store array with eye_array

---
+++ eye_track.py
@@ -0,0 +1,75 @@
+import cv2, dlib
+import numpy as np
+from imutils import face_utils
+from keras.models import load_model
+import time
+
+IMG_SIZE = (100, 100)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+
+def crop_eye(img, eye_points):
+    x1, y1 = np.amin(eye_points, axis=0)
+    x2, y2 = np.amax(eye_points, axis=0)
+    cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
+
+    w = (x2 - x1) * 1.2
+    h = w * IMG_SIZE[1] / IMG_SIZE[0]
+
+    margin_x, margin_y = w / 2, h / 2
+
+    min_x, min_y = int(cx - margin_x), int(cy - margin_y)
+    max_x, max_y = int(cx + margin_x), int(cy + margin_y)
+
+    eye_rect = np.rint([min_x, min_y, max_x, max_y]).astype(int)
+
+    eye_img = gray[eye_rect[1]:eye_rect[3], eye_rect[0]:eye_rect[2]]
+
+    return eye_img, eye_rect
+
+# main
+cap = cv2.VideoCapture(0)
+count = 0
+m = 50
+images_array = []
+
+# 5초에 50장 뽑아냄
+while count<m:
+    time.sleep(0.1)
+    ret, img_ori = cap.read()
+
+    if not ret:
+        raise Exception("캡처가 없음")
+
+    img_ori = cv2.resize(img_ori, dsize=(0, 0), fx=0.5, fy=0.5)
+
+    img = img_ori.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+
+    for face in faces:
+        shapes = predictor(gray, face)
+        shapes = face_utils.shape_to_np(shapes)
+        x = face.left()
+        y = face.top() #could be face.bottom() - not sure
+        w = face.right() - face.left()
+        h = face.bottom() - face.top()
+
+        eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
+        eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
+        ex,ey,ew,eh = eye_rect_l
+        eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+        eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
+        eye_img_r = cv2.flip(eye_img_r, flipCode=1)
+        cv2.rectangle(img, (ex,ey), (ew,eh), color=(255,255,255), thickness=1)
+        cv2.imwrite('./eyes_add/'+str(count)+'.jpg',img[ey:eh,ex:ew,:])
+        images_array.append(img[ey:eh,ex:ew,:])
+    count+=1
+
+
+    cv2.imshow('result', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# images_array => 5초에 50장 저장한 배열
