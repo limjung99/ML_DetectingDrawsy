d068649610613b567487f77735a4e0e2f9b187fb
lim <limjung99@naver.com>
2022-11-27 22:02:09 +0900

    주석추가

--- Second_model.py
+++ Second_model.py
@@ -6,7 +6,9 @@ from sklearn.model_selection import train_test_split,KFold
 from keras.models import Sequential, load_model
 from keras.layers import Dense
 
-# load data
+#load data
+#이미 저장해둔 video image 50장들의 배열과, 정답 label이 존재하면 directory로부터 불러온다.
+#없을경우 다음번 사용을 위해 directory에 저장
 def load_data():
     # 깨어있으면 1 자고있으면 0
     if os.path.isfile("videos_array2"):
@@ -28,14 +30,16 @@ def load_data():
     
     return videos_arr,videos_label
     
-
+#2차 모델 학습을 위해, Dataset이미지들의 눈 상태를 판정하여 그 배열을 return하는 함수
 def make_predicted():
-    predicted = [] #예측한 눈동자 상태 배열이 안에 append 된다.
+    predicted = [] #예측한 눈의 상태 배열이 안에 append 된다.
+    #이미 예측해두었던 dictionary가 directory에 존재한다면 불러온다. 
     if os.path.isfile("predicted"):
         with open("predicted","rb") as f:
             predicted = pickle.load(f)
         f.close()
     else:
+        #모델이 존재하지않을경우 모델을 만들고, 판정
         x_train, x_test, y_train, y_test = First_model.load_data()
         epochs = 100
         batch_sizes = 10
@@ -49,19 +53,10 @@ def make_predicted():
 
     return predicted
 
-
+#2차 학습모델을 만든다
 def make_model(predicted,ans):
-    # #train 및 test데이터 분리 
+    # #train 및 test데이터 분리
     x_train, x_test, y_train, y_test = train_test_split(np.array(predicted), np.array(ans), test_size=0.2)
-   
-
-
-    # Define the K-fold Cross Validator
-    #kfold = KFold(n_splits=5, shuffle=True)
-
-    #K-fold Cross Validation model evaluation
-    #fold_no = 1
-
     model_file = 'fitted_second_model.h5'
     if os.path.isfile(model_file):
         second_model = load_model('fitted_second_model.h5')
@@ -90,13 +85,14 @@ def make_model(predicted,ans):
             acc_per_fold.append(scores[1] * 100)
             loss_per_fold.append(scores[0])
             fold_no = fold_no + 1
+        #평균 손실과 정확도를 console에 log로 남긴다.
         print("K=",k," average accuracy=",sum(acc_per_fold)/len(acc_per_fold))
         print("K=",k," average loss=",sum(loss_per_fold)/len(loss_per_fold))
         second_model.save('fitted_second_model.h5')
     
     return second_model
 
-
+#최종적으로 졸음인지 깨어있는지 판단한다
 def drawsy_prediction(images_prediction):
     videos_arr, videos_label = load_data()
     predicted = make_predicted()
