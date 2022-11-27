89709c5e233f61199fcd250d9313c5b7b70d83bc
lim <limjung99@naver.com>
2022-11-27 18:58:01 +0900

    Kfold추가

--- Second_model.py
+++ Second_model.py
@@ -2,7 +2,7 @@ import First_model
 import pickle
 import numpy as np
 import os 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,KFold
 from keras.models import Sequential, load_model
 from keras.layers import Dense
 
@@ -53,8 +53,7 @@ def make_predicted():
 def make_model(predicted,ans):
     # #train 및 test데이터 분리 
     x_train, x_test, y_train, y_test = train_test_split(np.array(predicted), np.array(ans), test_size=0.2)
-    # validation 데이터 분리 
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2)
+   
 
 
     # Define the K-fold Cross Validator
@@ -72,16 +71,29 @@ def make_model(predicted,ans):
         print("여기")
         acc_per_fold=[]
         loss_per_fold=[]
-        second_model = Sequential()
-        second_model.add(Dense(50, activation='relu', input_shape=(50,))) 
-        second_model.add(Dense(50, activation='relu')) 
-        second_model.add(Dense(25,activation="relu"))
-        second_model.add(Dense(10, activation='relu'))
-        second_model.add(Dense(2, activation='softmax'))
-        #손실함수 binary_crossentropy 
-        second_model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        #binary_crossentropy -> sparse_categorical_crossentropy 로변경, 왜 와이? 아웃풋이 2개니까 
-        history = second_model.fit(x_train, y_train, epochs=100, batch_size=5,validation_data=(x_val,y_val))
+        k=5
+        kfold = KFold(n_splits=k,shuffle=True)
+        fold_no=1
+        for train_idx, test_idx in kfold.split(x_train, y_train):
+            second_model = Sequential()
+            second_model.add(Dense(50, activation='relu', input_shape=(50,))) 
+            second_model.add(Dense(50, activation='relu')) 
+            second_model.add(Dense(25,activation="relu"))
+            second_model.add(Dense(10, activation='relu'))
+            second_model.add(Dense(2, activation='softmax'))
+            
+            second_model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+            
+            #train
+            history = second_model.fit(x_train[train_idx], y_train[train_idx], epochs=100, batch_size=5)
+
+            scores = second_model.evaluate(x_train[test_idx], y_train[test_idx], verbose=0)
+            print(f'Score for fold {fold_no}: {second_model.metrics_names[0]} of {scores[0]}; {second_model.metrics_names[1]} of {scores[1]*100}%')
+            acc_per_fold.append(scores[1] * 100)
+            loss_per_fold.append(scores[0])
+            fold_no = fold_no + 1
+        print("K=",k," average accuracy=",sum(acc_per_fold)/len(acc_per_fold))
+        print("K=",k," average loss=",sum(loss_per_fold)/len(loss_per_fold))
         second_model.save('fitted_second_model.h5')
     
     return second_model
@@ -94,14 +106,8 @@ def drawsy_prediction(images_prediction):
     now_drawsy = np.argmax(second_model.predict(np.array(images_prediction)),axis=-1)
     print("-------------now_drawsy 반환-------------")
     return now_drawsy
-#for train, test in kfold.split(x_train, y_train):
-# model.save('is_sleeping.h5')
-# model2 = model
-# Generate generalization metrics
-# scores = model.evaluate(x_train[test], y_train[test], verbose=0)
-# print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
-# acc_per_fold.append(scores[1] * 100)
-# loss_per_fold.append(scores[0])
 
-# Increase fold number
-# fold_no = fold_no + 1
+
+
+
+
